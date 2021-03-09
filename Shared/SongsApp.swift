cbb68cbf2f04e52e57cb6959e6695991e1fecb88
Damiaan Dufaux <damiaan@dpro.be>
2021-03-09 17:42:47 +0100

    Initial Commit

---
+++ Shared/SongsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongsApp.swift
+//  Shared
+//
+//  Created by Damiaan on 09/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SongsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
