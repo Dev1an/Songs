5b646b45b471932fcaf163e2cc253cb60a99e10c
Damiaan Dufaux <damiaan@dpro.be>
2021-03-11 23:24:18 +0100

    propagate theme selection from ui to viewModel

--- iOS/Themes/ThemesController.swift
+++ iOS/Themes/ThemesController.swift
@@ -16,16 +16,18 @@ class ThemeController<Registry: SongRegistry>: CollectionController<ThemeControl
 		collection.view.delegate = self
 		navigationItem.title = "Themes"
 
-		selectionObserver = context.$selectedThemes.sink(receiveValue: { [unowned self] (selection) in
+		selectionObserver = context.$selectedThemes.sink { [unowned self] selection in
 			for themeID in selection {
 				let theme = context.registry[themeID]!
 				let path = collection.data.indexPath(for: theme)
 				collection.view.selectItem(at: path, animated: false, scrollPosition: .top)
 			}
-		})
+		}
 	}
 
 	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-		print(indexPath)
+		if let theme = collection.data.itemIdentifier(for: indexPath) {
+			context.selectedThemes = [theme.id]
+		}
 	}
 }
