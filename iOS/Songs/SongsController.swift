9cd9832d1fdb3bcad8ad290fff21816927be4b25
Damiaan Dufaux <damiaan@dpro.be>
2021-03-13 15:52:12 +0100

    propagate searchTerm from view to viewmodel

--- iOS/Songs/SongsController.swift
+++ iOS/Songs/SongsController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Combine
 
-class SongsController<Registry: PresentableSongRegistry>: CollectionController<SongsController>, UISearchResultsUpdating, UISearchControllerDelegate {
+class SongsController<Registry: PresentableSongRegistry>: CollectionController<SongsController>, UISearchResultsUpdating, UISearchControllerDelegate, UISearchBarDelegate, UICollectionViewDelegate {
 
 	let searchController = UISearchController(searchResultsController: nil)
 	var selectionObserver: AnyCancellable?
@@ -16,11 +16,8 @@ class SongsController<Registry: PresentableSongRegistry>: CollectionController<S
 	var themeObserver: AnyCancellable?
 
 	override func configureCollection() {
-		searchController.obscuresBackgroundDuringPresentation = false
-		searchController.searchResultsUpdater = self
-		searchController.delegate = self
-		searchController.view.layer.zPosition = collection.view.layer.zPosition + 1
-		navigationItem.searchController = searchController
+		collection.view.delegate = self
+		setupSearchBar()
 		navigationItem.title = "Songs"
 
 		themeObserver = context.$selectedThemes.sink { [unowned self] selection in
@@ -29,6 +26,7 @@ class SongsController<Registry: PresentableSongRegistry>: CollectionController<S
 			} else {
 				navigationItem.title = "Songs"
 			}
+			updateSearchScopeOptions(selection: selection)
 		}
 
 		selectionObserver = context.$selectedSongs.sink { [unowned self] selection in
@@ -50,16 +48,71 @@ class SongsController<Registry: PresentableSongRegistry>: CollectionController<S
 		}
 	}
 
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		if let song = collection.data.itemIdentifier(for: indexPath) {
+			context.selectedSongs = [song.id]
+			// TODO: navigate to song on compact width devices
+//			delegate?.navigateToTheme()
+		} else {
+			fatalError("Song not found")
+		}
+
+	}
+
+	// MARK: Search bar
+
+	func setupSearchBar() {
+		searchController.obscuresBackgroundDuringPresentation = false
+		searchController.searchResultsUpdater = self
+		searchController.delegate = self
+		searchController.searchBar.delegate = self
+		searchController.view.layer.zPosition = collection.view.layer.zPosition + 1
+		navigationItem.searchController = searchController
+	}
+
 	func updateSearchResults(for searchController: UISearchController) {
-		print("search")
+		if let searchTerm = searchController.searchBar.text {
+			context.searchTerm = searchTerm
+		}
+	}
+
+	func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+		print("scope", selectedScope)
+	}
+
+	func updateSearchScopeOptions(selection: Set<Registry.Theme.ID>) {
+		let themeNames: [String] = selection.compactMap { id in
+			guard let theme = context.registry[id] else { return nil }
+			return theme.subtitle ?? theme.title
+		}
+		let allSongs = NSLocalizedString("All songs", comment: "Search scope to searhc in all the songs")
+		searchController.searchBar.scopeButtonTitles = themeNames.isEmpty ? nil : [allSongs] + themeNames
 	}
 
 	func willPresentSearchController(_ searchController: UISearchController) {
 		let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
 		collection.view.setCollectionViewLayout(UICollectionViewCompositionalLayout.list(using: configuration), animated: true)
+
+		forceSelectionRedraw()
+	}
+
+	// TODO: remove this bugfix
+	func forceSelectionRedraw() {
+		let selection = context.selectedSongs.compactMap{context.registry[$0]}
+		if !selection.isEmpty {
+			var snapshot = collection.data.snapshot()
+			snapshot.reloadItems(selection)
+			collection.data.apply(snapshot)
+			for path in selection.compactMap({collection.data.indexPath(for: $0)}) {
+				collection.view.selectItem(at: path, animated: false, scrollPosition: [])
+			}
+		} else {
+			print("selection was empty", context.selectedSongs, selection)
+		}
 	}
 
 	func willDismissSearchController(_ searchController: UISearchController) {
 		collection.view.setCollectionViewLayout(Self.createLayout(), animated: true)
+		forceSelectionRedraw()
 	}
 }
